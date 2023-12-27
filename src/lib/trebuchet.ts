efb79ac967418abfb9d1611c77cebdd74c406d25
Kuba Birecki <kuba.birecki@crystalplanet.io>
2023-12-27 18:56:56 +0100

    Fix indentation

--- src/lib/trebuchet.ts
+++ src/lib/trebuchet.ts
@@ -3,20 +3,20 @@ import { sum } from './util';
 const PLAIN_DIGITS = [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' ];
 const SPELLED_OUT_DIGITS = [ 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' ];
 
-export const parseInput = ( input: string ): string[] =>
+export const parseInput = ( input: string ) =>
 	input.split( '\n' );
 
-const matchRowNumbers = ( patterns: string[] ) => ( line: string ): number => {
+const matchRowNumbers = ( patterns: string[] ) => ( line: string ) => {
 	const first = line.match( new RegExp( `(${ patterns.join( '|' ) })` ) )![ 1 ];
 	const last = line.match( new RegExp( `.*(${ patterns.join( '|' ) })` ) )![ 1 ];
 
-    return ( ( patterns.indexOf( first ) + 1 ) % 10 ) * 10 + ( patterns.indexOf( last ) + 1 ) % 10;
+	return ( ( patterns.indexOf( first ) + 1 ) % 10 ) * 10 + ( patterns.indexOf( last ) + 1 ) % 10;
 };
 
-export const sumOfDigits = ( lines: string[], patterns: string[] = PLAIN_DIGITS ): number =>
+export const sumOfDigits = ( lines: string[], patterns: string[] = PLAIN_DIGITS ) =>
 	lines
 		.map( matchRowNumbers( patterns ) )
 		.reduce( sum );
 
-export const sumIncludingSpelledDigits = ( lines: string[] ): number =>
+export const sumIncludingSpelledDigits = ( lines: string[] ) =>
 	sumOfDigits( lines, [ ...PLAIN_DIGITS, ...SPELLED_OUT_DIGITS ] );
